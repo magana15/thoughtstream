826f505573b6292a1769ad60287fe0feef3577da
joel magana <joelmagana15@gmail.com>
2024-10-29 15:53:03 +0300

    finalising the presentation by checking the code structure and arrangements

--- routes.py
+++ routes.py
@@ -135,6 +135,14 @@ def init_routes(app, db, bcrypt, cache):
     @app.route('/post/new', methods=['GET', 'POST'])
     @login_required
     def new_post():
+        """
+        The `new_post` function in Python handles the creation of a new blog post, including image
+        upload, form validation, and database insertion.
+        :return: The `new_post` function returns either a redirect to the 'new_post' route if there are
+        missing required fields (title, image, content), or a redirect to the 'post_detail' route with
+        the newly created post's ID if the post is successfully created and saved in the database. If
+        the request method is not 'POST', it returns the 'create_post.html' template for rendering the
+        """
         if request.method == 'POST':
             if 'blog_image' in request.files:
                 blog_image = request.files['blog_image']
@@ -142,7 +150,7 @@ def init_routes(app, db, bcrypt, cache):
                     filename = secure_filename(blog_image.filename)
                     filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
                 
-                    # Save the profile photo to the specified folder
+                    # Save the profile photo to the uploads folder
                     blog_image.save(filepath)
                 
                     # Update the user's profile photo in the database
@@ -168,6 +176,13 @@ def init_routes(app, db, bcrypt, cache):
 #reading posts
     @app.route('/posts')
     def posts():
+        """
+        This Python function retrieves and paginates posts from a database and renders them on a
+        webpage.
+        :return: The `posts()` function returns either a rendered template 'posts.html' with the posts
+        fetched from the database and paginated, or it returns a rendered template '404.html' with a 404
+        status code if there are no posts found.
+        """
         
         page = request.args.get('page', 1, type=int)
         all_posts = Post.query.order_by(Post.date_posted.desc()).paginate(page=page, per_page=5)
@@ -178,6 +193,18 @@ def init_routes(app, db, bcrypt, cache):
 # Viewing a single post
     @app.route('/post/<int:post_id>')
     def post_detail(post_id):
+        """
+        This Python function retrieves details of a specific post and renders them in a template for
+        display.
+        
+        :param post_id: The `post_id` parameter in the `post_detail` function is used to identify the
+        specific post for which the details are being requested. It is typically an identifier (such as
+        a unique numerical ID) that is used to retrieve the corresponding post from a database or data
+        source
+        :return: The `post_detail` function is returning the rendered template 'post_detail.html' with
+        the `post` variable passed to it. The `post` variable contains the Post object retrieved from
+        the database with the given `post_id`.
+        """
         page = request.args.get('page', 1, type=int)
         post = Post.query.get_or_404(post_id)
         return render_template('post_detail.html', post=post)
@@ -185,6 +212,16 @@ def init_routes(app, db, bcrypt, cache):
     @app.route('/post/<int:post_id>/edit', methods=['GET', 'POST'])
     @login_required
     def edit_post(post_id):
+        """
+        The function `edit_post` allows a user to edit a post if they are the author of the post.
+        
+        :param post_id: The `post_id` parameter is the unique identifier of the post that you want to
+        edit. It is used to retrieve the specific post from the database so that it can be edited
+        :return: The `edit_post` function returns either a redirect to the `post_detail` page for the
+        edited post if the request method is POST and the post is successfully updated in the database,
+        or it returns the rendered template 'edit_post.html' with the post data if the request method is
+        not POST.
+        """
         post = Post.query.get_or_404(post_id)
 
         # Check if the current user is the author of the post
@@ -204,6 +241,17 @@ def init_routes(app, db, bcrypt, cache):
     @app.route('/post/<int:post_id>/delete', methods=['POST'])
     @login_required
     def delete_post(post_id):
+        """
+        The function `delete_post` deletes a post and its associated comments if the current user is the
+        author of the post.
+        
+        :param post_id: The `post_id` parameter is the unique identifier of the post that you want to
+        delete from the database. It is used to locate the specific post record in the database and
+        perform the deletion operation
+        :return: The `delete_post` function is returning a redirect response to the 'posts' route using
+        `redirect(url_for('posts'))`. This means that after deleting the post and its associated
+        comments from the database, the user will be redirected to the 'posts' page.
+        """
         post = Post.query.get_or_404(post_id)
 
         # Check if the current user is the author of the post
@@ -220,9 +268,20 @@ def init_routes(app, db, bcrypt, cache):
     
 
     @app.route('/profile/<int:user_id>')
-    @app.route('/profile')
+    
     @login_required
     def profile(user_id):
+        """
+        The function `profile` retrieves a user's information from the database and renders a template
+        to display the user's profile.
+        
+        :param user_id: The `user_id` parameter is the unique identifier of a user in the system. It is
+        used to retrieve the user's information from the database using the
+        `User.query.get_or_404(user_id)` method. This method queries the database for a user with the
+        specified `user_id` and returns
+        :return: The `profile` function is returning the rendered template 'profile.html' with the user
+        data passed to it as a context variable.
+        """
         user= User.query.get_or_404(user_id)
         return render_template('profile.html', user=user)
 
@@ -230,6 +289,10 @@ def init_routes(app, db, bcrypt, cache):
     @app.route('/profile/edit', methods=['GET', 'POST'])
     @login_required
     def edit_profile():
+        """
+        The `edit_profile` function handles updating a user's profile information, including their
+        profile photo, username, and email.
+        """
         user= current_user
         if request.method == 'POST':
             if 'profile_photo' in request.files:
@@ -252,13 +315,23 @@ def init_routes(app, db, bcrypt, cache):
 
             db.session.commit()
             flash('Your profile has been updated.', 'success')
-            return redirect(url_for('profile'))
+            
+            return render_template('profile.html', user=user)
 
         return render_template('edit_profile.html', user=user)
 
     #searching
     @app.route('/search')
     def search():
+        """
+        The `search` function retrieves search query from URL parameters, searches for matching posts in
+        the database, and displays the results on a template.
+        :return: The `search()` function returns the search results based on the query provided in the
+        URL parameters. It searches for posts that match the query in the title or body of the post, as
+        well as in the email of the user who created the post. If no query is provided, it redirects to
+        the homepage. Finally, it renders a template `search_results.html` with the query and the search
+        results
+        """
         author = User.username
         query = request.args.get('query')  # Get the search term from the URL parameters
         if not query:
@@ -272,6 +345,11 @@ def init_routes(app, db, bcrypt, cache):
     #about page
     @app.route('/about')
     def about():
+        """
+        This function retrieves a post with ID 1 from the database and renders the 'about.html'
+        template.
+        :return: The `about()` function is returning the rendered template 'about.html'.
+        """
         post = Post.query.get(1)
         return render_template('about.html')
     
@@ -279,6 +357,15 @@ def init_routes(app, db, bcrypt, cache):
     @app.route('/post/<int:post_id>/comment', methods=['POST'])
     @login_required
     def add_comment(post_id):
+        """
+        The function `add_comment` adds a new comment to a post in a web application.
+        
+        :param post_id: The `post_id` parameter is the unique identifier of the post to which the
+        comment will be added. It is used to retrieve the specific post from the database to which the
+        comment will be associated
+        :return: a redirect response to the 'post_detail' route with the post_id parameter set to the id
+        of the post that the comment was added to.
+        """
         post = Post.query.get_or_404(post_id)
         content = request.form['content']
 
@@ -293,6 +380,16 @@ def init_routes(app, db, bcrypt, cache):
     @app.route('/post/<int:post_id>/like', methods=['POST'])
     @login_required
     def like_post(post_id):
+        """
+        This Python function allows a user to like or unlike a post on a social media platform.
+        
+        :param post_id: The `post_id` parameter in the `like_post` function is used to identify the post
+        that the user wants to like or unlike. It is the unique identifier of the post in the database.
+        The function retrieves the post with the given `post_id` from the database and then checks if
+        the
+        :return: The `like_post` function returns a redirect response to the `post_detail` route with
+        the `post_id` parameter set to `post.id`.
+        """
         post = Post.query.get_or_404(post_id)
 
         # Check if the user has already liked the post
@@ -342,12 +439,31 @@ def init_routes(app, db, bcrypt, cache):
     
     @app.route('/uploads/<filename>')
     def uploaded_file(filename):
+        """
+        The function `uploaded_file` retrieves a file from the specified directory and sends it to the
+        user.
+        
+        :param filename: The `filename` parameter is a string that represents the name of the file that
+        has been uploaded by the user
+        :return: the file specified by the `filename` parameter from the directory specified by
+        `app.config['UPLOAD_FOLDER']`.
+        """
         user = current_user
         return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
     @app.route('/favorite/<int:post_id>', methods=['POST'])
     @login_required
     def favorite_post(post_id):
+        """
+        The function `favorite_post` adds a post to the current user's favorites if it is not already
+        there and displays a message accordingly.
+        
+        :param post_id: The `post_id` parameter in the `favorite_post` function is used to identify the
+        specific post that the user wants to add to their favorites. This function retrieves the post
+        with the given `post_id` from the database using `Post.query.get_or_404(post_id)`. If the post
+        :return: The `favorite_post` function is returning a redirect response to the 'posts' route
+        using `url_for('posts')`.
+        """
         post = Post.query.get_or_404(post_id)
         if post not in current_user.favorites:
             current_user.favorites.append(post)
@@ -360,6 +476,16 @@ def init_routes(app, db, bcrypt, cache):
     @app.route('/unfavorite/<int:post_id>', methods=['POST'])
     @login_required
     def unfavorite_post(post_id):   
+        """
+        The function `unfavorite_post` removes a post from the current user's favorites list if it
+        exists, and provides feedback messages accordingly.
+        
+        :param post_id: The `post_id` parameter is the unique identifier of the post that the user wants
+        to unfavorite. It is used to retrieve the specific post from the database and remove it from the
+        user's list of favorite posts
+        :return: The `unfavorite_post` function is returning a redirect response to the 'posts' route
+        using `url_for('posts')`.
+        """
         post = Post.query.get_or_404(post_id)
         if post in current_user.favorites:
             current_user.favorites.remove(post)
@@ -373,12 +499,14 @@ def init_routes(app, db, bcrypt, cache):
     @app.route('/favorites')
     @login_required
     def favorites():
+        """
+        The `favorites` function retrieves the current user's favorite posts and renders them in the
+        'favorites.html' template.
+        :return: The function `favorites()` is returning a rendered template called 'favorites.html'
+        with the user's favorite posts passed as a variable named 'posts'.
+        """
         favorite_posts = current_user.favorites  # Retrieve user's favorite posts
         return render_template('favorites.html', posts=favorite_posts)
-
-
-
-
     #error handlers
     @app.errorhandler(404)
     def not_found_error(error):
